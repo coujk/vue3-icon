e9b86f0d85aff9e220c87059d51f8124ae8d90ee
wangyachao <wangyachao@metamaker.cn>
2022-10-10 16:51:55 +0800

    pinyin

--- bin/utils.js
+++ bin/utils.js
@@ -1,16 +1,16 @@
-const upperCamelCase = require('uppercamelcase')
-
+const upperCamelCase = require("uppercamelcase");
+import { pinyin } from "pinyin-pro";
 const parseName = (name, defaultStyle) => {
-  const cleanedName = name.replace(/\//g, '-')
-  const nameSlices = cleanedName.split('-')
-  const style = nameSlices[nameSlices.length - 1]
+  const cleanedName = name.replace(/\//g, "-");
+  const nameSlices = cleanedName.split("-");
+  const style = nameSlices[nameSlices.length - 1];
   return {
     name,
-    componentName: upperCamelCase(cleanedName),
-    style: style==='fill' || style==='stroke' ? style : defaultStyle
-  }
-}
+    componentName: upperCamelCase(pinyin(cleanedName, { toneType: 'none' })),
+    style: style === "fill" || style === "stroke" ? style : defaultStyle,
+  };
+};
 
 module.exports = {
-  parseName
+  parseName,
 };
